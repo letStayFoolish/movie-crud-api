e9499a6d12468d52d74758d30647f29a2032e637
stayFoolish <nemanjakaraklajic90@icloud.com>
2025-09-11 20:27:59 +0200

    Consistently reformat `MovieDbContext` to improve code readability and adherence to code style standards.

--- MovieApi/Persistence/MovieDbContext.cs
+++ MovieApi/Persistence/MovieDbContext.cs
@@ -5,40 +5,41 @@ namespace MovieApi.Persistence;
 
 public class MovieDbContext(DbContextOptions<MovieDbContext> options) : DbContext(options)
 {
- public DbSet<Movie> Movies { get; set; }
+    public DbSet<Movie> Movies { get; set; }
 
- protected override void OnModelCreating(ModelBuilder modelBuilder)
- {
-  modelBuilder.HasDefaultSchema("app");
-  modelBuilder.ApplyConfigurationsFromAssembly(typeof(MovieDbContext).Assembly);
-  base.OnModelCreating(modelBuilder);
- }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("app");
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MovieDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
 
- protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
- {
-  optionsBuilder
-   .UseAsyncSeeding(async (context, _, cancellationToken) =>
-   {
-    var sampleMovie = await context.Set<Movie>()
-     .FirstOrDefaultAsync(b => b.Title == "Sonic the Hedgehog 3", cancellationToken);
-    if (sampleMovie is null)
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-     sampleMovie = Movie.Create("Sonic the Hedgehog 3", "Fantasy", new DateTimeOffset(new DateTime(2025,1,3), TimeSpan.Zero), 9.5);
-     await context.Set<Movie>().AddAsync(sampleMovie, cancellationToken);
-     await context.SaveChangesAsync(cancellationToken);
+        optionsBuilder
+            .UseAsyncSeeding(async (context, _, cancellationToken) =>
+            {
+                var sampleMovie = await context.Set<Movie>()
+                    .FirstOrDefaultAsync(b => b.Title == "Sonic the Hedgehog 3", cancellationToken);
+                if (sampleMovie is null)
+                {
+                    sampleMovie = Movie.Create("Sonic the Hedgehog 3", "Fantasy",
+                        new DateTimeOffset(new DateTime(2025, 1, 3), TimeSpan.Zero), 9.5);
+                    await context.Set<Movie>().AddAsync(sampleMovie, cancellationToken);
+                    await context.SaveChangesAsync(cancellationToken);
+                }
+            })
+            .UseSeeding((context, _) =>
+            {
+                var sampleMovie = context.Set<Movie>()
+                    .FirstOrDefault(b => b.Title == "Sonic the Hedgehog 3");
+                if (sampleMovie is null)
+                {
+                    sampleMovie = Movie.Create("Sonic the Hedgehog 3", "Fantasy",
+                        new DateTimeOffset(new DateTime(2025, 1, 3), TimeSpan.Zero), 9.5);
+                    context.Set<Movie>().Add(sampleMovie);
+                    context.SaveChanges();
+                }
+            });
     }
-   })
-   .UseSeeding((context, _) =>
-   {
-    var sampleMovie = context.Set<Movie>()
-     .FirstOrDefault(b => b.Title == "Sonic the Hedgehog 3");
-    if (sampleMovie is null)
-    {
-     sampleMovie = Movie.Create("Sonic the Hedgehog 3", "Fantasy",
-      new DateTimeOffset(new DateTime(2025, 1, 3), TimeSpan.Zero), 9.5);
-     context.Set<Movie>().Add(sampleMovie);
-     context.SaveChanges();
-    }
-   });
- }
 }
