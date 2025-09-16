14a0b8d258c82c97d9683a222c26e4567f002aea
stayFoolish <nemanjakaraklajic90@icloud.com>
2025-09-16 21:14:33 +0200

    Add validation attributes to DTOs, enhance `ValidateModelFilter` with logging, and update `MoviesController` for custom model validation handling

--- MovieApi/DTOs/Movie/CreateMovieDto.cs
+++ MovieApi/DTOs/Movie/CreateMovieDto.cs
@@ -1,3 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MovieApi.DTOs.Movie;
 
-public record CreateMovieDto(string Title, string Genre, DateTimeOffset ReleaseDate, double Rating);
+public record CreateMovieDto(
+    [param: Required(AllowEmptyStrings = false), MinLength(1), MaxLength(200)]
+    string Title,
+
+    [param: Required(AllowEmptyStrings = false), MinLength(1), MaxLength(100)]
+    string Genre,
+
+    [param: Required]
+    DateTimeOffset ReleaseDate,
+
+    [param: Range(0, 10)]
+    double Rating
+);
+
