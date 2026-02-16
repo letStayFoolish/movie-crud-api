70301751c4d1251a4aa195e98e6e1f82cdea7c33
stayFoolish <nemanjakaraklajic90@icloud.com>
2026-02-16 15:08:50 +0100

    Add migration for `Order`, `OrderItem`, `Product`, `Customer`, `Tag`, and related configurations

---
+++ MovieApi/Migrations/20260216140637_AddEntitites_Order_Product_Customer_Tag_OrderItems.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEntitites_Order_Product_Customer_Tag_OrderItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "customer_profiles",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    ShippingAddress = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer_profiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_customer_profiles_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalSchema: "app",
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_orders_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalSchema: "app",
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "product_tags",
+                schema: "app",
+                columns: table => new
+                {
+                    ProductsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TagsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_product_tags", x => new { x.ProductsId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_product_tags_Product_ProductsId",
+                        column: x => x.ProductsId,
+                        principalSchema: "app",
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_product_tags_Tag_TagsId",
+                        column: x => x.TagsId,
+                        principalSchema: "app",
+                        principalTable: "Tag",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order_items",
+                schema: "app",
+                columns: table => new
+                {
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Discount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_order_items", x => new { x.OrderId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_order_items_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalSchema: "app",
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_order_items_orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "app",
+                        principalTable: "orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customer_profiles_CustomerId",
+                schema: "app",
+                table: "customer_profiles",
+                column: "CustomerId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_items_ProductId",
+                schema: "app",
+                table: "order_items",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_CustomerId",
+                schema: "app",
+                table: "orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_tags_TagsId",
+                schema: "app",
+                table: "product_tags",
+                column: "TagsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customer_profiles",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "order_items",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "product_tags",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "orders",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "Product",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "Tag",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "Customer",
+                schema: "app");
+        }
+    }
+}
